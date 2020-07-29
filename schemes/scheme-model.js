85561c3c808f32faffa00f9381ee6409c3098580
Nathan Brown <scottynate05@yahoo.com>
2020-07-28 20:57:35 -0700

    built db-config.js file, and framed out my model file

---
+++ schemes/scheme-model.js
@@ -0,0 +1,29 @@
+const db = require('../data/db-config.js');
+
+function find() {
+    return db('schemes')
+}
+
+function findById() {
+
+}
+
+function add() {
+
+}
+
+function update() {
+
+}
+
+function remove() {
+
+}
+
+module.exports = {
+    find,
+    findById,
+    add,
+    update,
+    remove
+};
