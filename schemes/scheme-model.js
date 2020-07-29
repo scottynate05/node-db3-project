609429e28a4483f3f0d2a95246d8d8172e3dcde5
Nathan Brown <scottynate05@yahoo.com>
2020-07-29 00:55:40 -0700

    completed sql queries, mvp met

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,28 +1,42 @@
 const db = require('../data/db-config.js');
 
 function find() {
+    return db('schemes');
+}
+
+function findById(id) {
     return db('schemes')
+        .where({ id });
 }
 
-function findById() {
-
+function findSteps(id) {
+    return db('steps as st')
+        .join('schemes as sc', 'sc.id', 'st.scheme_id')
+        .select('st.id', 'sc.scheme_name', 'st.step_number','st.instructions')
+        .where({ 'st.scheme_id': id});
 }
 
-function add() {
-
+function add(schemeData) {
+    return db('schemes')
+        .insert(schemeData)
 }
 
-function update() {
-
+function update(changes, id) {
+    return db('schemes')
+        .where({ id })
+        .update(changes);
 }
 
-function remove() {
-
+function remove(id) {
+    return db('schemes')
+        .where({ id })
+        .delete()
 }
 
 module.exports = {
     find,
     findById,
+    findSteps,
     add,
     update,
     remove
